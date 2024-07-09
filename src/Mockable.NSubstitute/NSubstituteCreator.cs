3545b79dd248ef2289074b2208a7c28f130f8ddf
Dean Dashwood <dean@dashwood.world>
2024-07-09 08:49:23 +0100

    Add NSubstitute code

---
+++ src/Mockable.NSubstitute/NSubstituteCreator.cs
@@ -0,0 +1,29 @@
+﻿using Mockable.Core;
+using Mockable.Core.Exceptions;
+using NSubstitute;
+
+namespace Mockable.NSubstitute;
+
+internal class NSubstituteCreator : IMockCreator
+{
+    public object GetMockOf(Type type, out object mockConfigurator)
+    {
+        var substituteType = typeof(Substitute);
+        var forMethod = substituteType.GetMethod("For");
+        if (forMethod == null)
+        {
+            throw new MockableException("Error getting Substitute.For<> method");
+        }
+
+        var forGenericMethod = forMethod.MakeGenericMethod(type);
+
+        var mock = forGenericMethod.Invoke(null, Array.Empty<object?>());
+        if (mock == null)
+        {
+            throw new MockableException($"Failed to get mock for {type.FullName}");
+        }
+
+        mockConfigurator = mock;
+        return mock;
+    }
+}
---
+++ src/Mockable.NSubstitute/ServiceFactory.cs
@@ -0,0 +1,16 @@
+﻿using Mockable.Core;
+
+namespace Mockable.NSubstitute;
+
+/// <summary>
+/// A Service Factory for creating services, using NSubstitute
+/// for mocking dependencies
+/// </summary>
+public class ServiceFactory : ServiceFactoryBase
+{
+    /// <inheritdoc />
+    public ServiceFactory()
+        :base(new NSubstituteCreator())
+    {
+    }
+}
