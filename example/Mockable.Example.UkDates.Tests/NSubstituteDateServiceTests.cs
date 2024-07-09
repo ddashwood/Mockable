09a30d7f81d59c8f02480383a0acd386a1099819
Dean Dashwood <dean@dashwood.world>
2024-07-09 09:14:26 +0100

    Full set of NSubstitute tests for the example app

--- example/Mockable.Example.UkDates.Tests/NSubstituteDateServiceTests.cs
+++ example/Mockable.Example.UkDates.Tests/NSubstituteDateServiceTests.cs
@@ -1,5 +1,8 @@
-﻿using Mockable.Example.UkDates.Services;
+﻿using Mockable.Example.UkDates.Models;
+using Mockable.Example.UkDates.Services;
+using Mockable.Example.UkDates.Tests.Configurators;
 using Mockable.NSubstitute;
+using NSubstitute;
 
 namespace Mockable.Example.UkDates.Tests;
 
@@ -17,5 +20,78 @@ public class NSubstituteDateServiceTests
 
         // Assert
         Assert.Equal("The date supplied is not a valid date", result);
+    }
+
+    [Fact]
+    public async Task NoBankHolidaysTest()
+    {
+        // Arrange
+        var serviceFactory = new ServiceFactory();
+        var service = serviceFactory.Create<DateService, FakeItEasyDateServiceConfigurators>(out var configurators);
+        configurators.BankHolidaysService.GetBankHolidaysAsync().Returns((BankHolidayCollection?)null);
+
+        // Act
+        var result = await service.GetDateDescriptionAsync(2024, 2, 1);
+
+        // Assert
+        Assert.Equal("1 February 2024 is a Thursday. There was a problem fetching the bank holidays.", result);
+    }
+
+    [Fact]
+    public async Task BankHolidayTest()
+    {
+        // Arrange
+        var serviceFactory = new ServiceFactory();
+        var service = serviceFactory.Create<DateService, FakeItEasyDateServiceConfigurators>(out var configurators);
+
+        var bankHolidays = new BankHolidayCollection
+        {
+            englandandwales = new Division
+            {
+                events = new Event[]
+                {
+                    new Event { date = new DateOnly(2023, 12, 25), title = "Christmas Day"}
+                }
+            },
+            scotland = new Division { events = new Event[0] },
+            northernireland = new Division { events = new Event[0] }
+        };
+
+        configurators.BankHolidaysService.GetBankHolidaysAsync().Returns(bankHolidays);
+
+        // Act
+        var result = await service.GetDateDescriptionAsync(2023, 12, 25);
+
+        // Assert
+        Assert.Equal("25 December 2023 is a Monday. In England and Wales, this is Christmas Day.", result);
+    }
+
+    [Fact]
+    public async Task BankHolidayWithNoteTest()
+    {
+        // Arrange
+        var serviceFactory = new ServiceFactory();
+        var service = serviceFactory.Create<DateService, FakeItEasyDateServiceConfigurators>(out var configurators);
+
+        var bankHolidays = new BankHolidayCollection
+        {
+            englandandwales = new Division
+            {
+                events = new Event[]
+                {
+                    new Event { date = new DateOnly(2022, 12, 27), title = "Christmas Day", notes = "Substitute"}
+                }
+            },
+            scotland = new Division { events = new Event[0] },
+            northernireland = new Division { events = new Event[0] }
+        };
+
+        configurators.BankHolidaysService.GetBankHolidaysAsync().Returns(bankHolidays);
+
+        // Act
+        var result = await service.GetDateDescriptionAsync(2022, 12, 27);
+
+        // Assert
+        Assert.Equal("27 December 2022 is a Tuesday. In England and Wales, this is Christmas Day (Substitute).", result);
     }
 }
